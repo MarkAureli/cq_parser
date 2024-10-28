49201ab63543fec95e55c007b6b4aa169c33b12c
LennartBinkowski <lennart.binkowski@itp.uni-hannover.de>
2024-10-28 11:58:04 +0100

    Create cq_parser.rb

---
+++ Formula/cq_parser.rb
@@ -0,0 +1,20 @@
+class MyProject < Formula
+  desc "Lexer and parser for the CQ programming language"
+  homepage "https://github.com/MarkAureli/cq_parser"
+  url "https://github.com/MarkAureli/cq_parser/archive/v1.0.0.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+
+  # Specify dependencies
+  depends_on "gcc"
+  depends_on "flex"
+  depends_on "bison"
+
+  def install
+    system "make"
+    bin.install "cq_parser"
+  end
+
+  test do
+    system "make", "test"
+  end
+end
