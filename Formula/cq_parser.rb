fe2171d2a161371df9b814ff3f88a4bba037255f
LennartBinkowski <lennart.binkowski@itp.uni-hannover.de>
2024-10-28 12:16:13 +0100

    Update cq_parser.rb (release url)

--- Formula/cq_parser.rb
+++ Formula/cq_parser.rb
@@ -1,7 +1,7 @@
 class CqParser < Formula
   desc "Lexer and parser for the CQ programming language"
   homepage "https://github.com/MarkAureli/cq_parser"
-  url "https://github.com/MarkAureli/cq_parser/archive/v1.0.0.tar.gz"
+  url "https://github.com/MarkAureli/cq_parser/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
 
   # Specify dependencies
