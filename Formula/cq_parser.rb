0a068e337bb24224d8a016a255314a7687822517
Lennart Binkowski <lennart.binkowski@yahoo.com>
2024-11-01 21:29:22 +0100

    updated checksum

--- Formula/cq_parser.rb
+++ Formula/cq_parser.rb
@@ -2,7 +2,7 @@ class CqParser < Formula
     desc "Lexer and parser for the CQ programming language"
     homepage "https://github.com/MarkAureli/cq_parser"
     url "https://github.com/MarkAureli/cq_parser/archive/refs/tags/v1.0.1.tar.gz"
-    sha256 "ae2ff4a6968aa157d9741ebab42408a292ff4039eb42c265a7a33c53844c146b"
+    sha256 "f701cd582fac2d4a87bdc7f8be0c1b8209cacc16d8ff9a68bd952d1d1f01ce3f"
 
     depends_on "flex"
     depends_on "bison"
