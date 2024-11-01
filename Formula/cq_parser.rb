7c99095f14fe803ebdfb1a81d29de0cffea62f39
Lennart Binkowski <lennart.binkowski@yahoo.com>
2024-11-01 20:59:36 +0100

    changed version number in formula

--- Formula/cq_parser.rb
+++ Formula/cq_parser.rb
@@ -1,7 +1,7 @@
 class CqParser < Formula
     desc "Lexer and parser for the CQ programming language"
     homepage "https://github.com/MarkAureli/cq_parser"
-    url "https://github.com/MarkAureli/cq_parser/archive/refs/tags/v1.0.0.tar.gz"
+    url "https://github.com/MarkAureli/cq_parser/archive/refs/tags/v1.0.1.tar.gz"
     sha256 "29c35b1ca079ecf61ebf3d4627031353ca7f47bc0974a48f9905afe54612af0f"
 
     depends_on "flex"
